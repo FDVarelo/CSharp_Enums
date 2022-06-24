0bd52ed06d6d904135b2cf85e3a0c74b536b70ab
FDVarelo <fdvarelo@gmail.com>
2022-06-24 16:53:04 -0300

    first commit

---
+++ Enum_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Enum_1.Entities;
+using Enum_1.Entities.Enums;
+using System;
+
+namespace Enum_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 2121,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
